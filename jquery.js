d65ae63ee537d4dd46ca0cffc073ec41bfdea66d
jdcalvin <calvjd@gmail.com>
2014-05-10 14:13:39 -0700

    formatted jquery examples

--- jquery.js
+++ jquery.js
@@ -1,13 +1,17 @@
 //descendant selector
 // $("#parent .child") Selects all children
+
 //child selector
 //$("#parent > .child") Selects only direct child, not childrens children
-//psuedo class
-//$("#parent .child:first") :last, :odd, :even, 
 
-//traversing the DOM
-$("#destinations li"); //NO
-$("#destinations").find("li"); //YES - its faster
+//psuedo class
+//$("#parent .child:first") -or- :last, :odd, :even
+
+//===================================================================
+//TRAVERSING THE DOM
+//===================================================================
+$("#destinations li"); //NO - Works but slow
+$("#destinations").find("li"); //YES - Querying DOM is faster
 
 $("li").first(); // works for other n objects
 
@@ -18,10 +22,12 @@ $("li").first().next().prev().next();
 $("li").first().parent();
 
 //WALK DOWN THE DOM
-$("#destinations").children("li") //this also works to select direct descents ie #destinations > li
+$("#destinations").children("li") 
+//this also works to select direct descents ie #destinations > li
 
-
+//===================================================================
 //WORKING WITH THE DOM
+//===================================================================
 
 //Appending to the DOM and REMOVING
 $(document).ready(function(){
@@ -29,16 +35,16 @@ $(document).ready(function(){
 	$('.vacation').append(price);
 	$('button').remove();
 });
-//.appendTo, .prepentTo, .insertAfter, .insertBefore
+//-or- .appendTo, .prepentTo, .insertAfter, .insertBefore
 
+//===================================================================
 //ACTING ON INTERACTION
-
+//===================================================================
 //event handler
 
 $(document).ready(function(){
 	//function runs when DOM is ready
 });
-
 $(document).ready(function(){
 	$('button').on('click', function(){
 		var price = $('<p>From $399.99</p>');
@@ -47,8 +53,9 @@ $(document).ready(function(){
 	})
 });
 
-//REFACTOR USING traversing
-
+//===================================================================
+//REFACTOR USING TRAVERSING
+//===================================================================
 //INTRO TO $(this)
 $(document).ready(function(){
 	$('button').on('click', function(){
@@ -58,13 +65,14 @@ $(document).ready(function(){
 
 	})
 });
-
+//===================================================================
 //Data attributes to HTML
+//===================================================================
 
+//Examle HTML
 <li class="vacation" data-price="399.99">
 
 $('vacation').first().data('price'); // .data(<name>,<value>)
-
 $(document).ready(function(){
 	$('button').on('click', function(){
 		var amount = vacation.data('price');
@@ -80,33 +88,35 @@ $(document).ready(function(){
 $('vacation').on('click', 'button', function(){}); 
 //$(this) = 'button' inside 'vacation'
 
-//FILTERING HTML - USE FOR CHART
-
-%('#filters').on('click', '.onsale-filter',function(){
-		$('highlighted').removeClass('highlighted'); //removes class before applying
+//===================================================================
+//FILTERING HTML
+//===================================================================
+$('#filters').on('click', '.onsale-filter',function(){
+		$('highlighted').removeClass('highlighted'); 
+		//removes class before applying (otherwise effects all elements)
 		$('vacation').filter('.onsale').addClass('highlighted');
 
 	});
-
+//===================================================================
 //LISTENING
+//===================================================================
 $(document).ready(function(){
-
-$('.confirmation').on('click', 'button', function() {
-	$(this).closest('.confirmation').find('.ticket').slideDown(); // need to wrap inside doc ready
-
+	//ensure event is wrapped in doc ready
+	$('.confirmation').on('click', 'button', function() {
+		$(this).closest('.confirmation').find('.ticket').slideDown();
+	});
 });
-});
-
-//Keyboard events
+//===================================================================
+//KEYBOARD EVENTS
 //keypress, keydown, keyup
-//Form events
+//FORM EVENTS
 //blur, select, change, focus, submit
-
+//===================================================================
 $(documents).ready(function(){
-	4('.vacation').on('keyup', '.quantity', function(){
+	$('.vacation').on('keyup', '.quantity', function(){
 		//var price = $(this).closest('.vacation').data(price);
 		//returns a string
-		//adding plus converts to integer
+		//adding '+' converts to integer
 		var price = +$(this).closest('.vacation').data(price);
 		var quantity = +$(this).val();
 	});
